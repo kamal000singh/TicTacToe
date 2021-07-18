140f8fb6a9583591211c3e04515f40149700bc9c
kamal-singh <kamal000rawat@gmail.com>
2021-07-18 17:30:40 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal.</p>
+    <h2>Hard to get more minimal</h2>
+    <ul>
+      <li>Kamal</li>
+      <li>Singh</li>
+      <li>Rawat</li>
+    </ul>
+  </>
+);
+}
+export default App;
